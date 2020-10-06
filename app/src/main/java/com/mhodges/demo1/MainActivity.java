b435309a8f134d90ea42ae5e37c283ec21496b40
Mike Hodges <mikehodges7@outlook.com>
2020-10-06 14:35:09 -0500

    Added Demo1 Files

---
+++ app/src/main/java/com/mhodges/demo1/MainActivity.java
@@ -0,0 +1,34 @@
+package com.mhodges.demo1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ((Button) findViewById(R.id.btnVibrate)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Vibrate(300);
+            }
+        });
+    }
+
+    private void Vibrate(int milliseconds) {
+        if (Build.VERSION.SDK_INT >= 26) {
+            ((Vibrator) getSystemService(VIBRATOR_SERVICE)).vibrate(VibrationEffect.createOneShot(milliseconds, VibrationEffect.DEFAULT_AMPLITUDE));
+        } else {
+            ((Vibrator) getSystemService(VIBRATOR_SERVICE)).vibrate(milliseconds);
+        }
+    }
+}
