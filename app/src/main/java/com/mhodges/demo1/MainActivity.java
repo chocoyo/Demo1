f6d7614f6f5b4aaafd7c4ccae9dc43cc2ae69c26
Mike Hodges <mikehodges7@outlook.com>
2020-10-08 12:45:30 -0500

    Added The Stuff I Forgot To Add

--- app/src/main/java/com/mhodges/demo1/MainActivity.java
+++ app/src/main/java/com/mhodges/demo1/MainActivity.java
@@ -1,5 +1,6 @@
 package com.mhodges.demo1;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Build;
@@ -8,6 +9,7 @@ import android.os.VibrationEffect;
 import android.os.Vibrator;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,19 +19,40 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ((Button) findViewById(R.id.btnVibrate)).setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.O)
             @Override
             public void onClick(View view) {
-                Vibrate(300);
+                Vibrate(VibrationEffect.createOneShot(300, VibrationEffect.EFFECT_HEAVY_CLICK));
+            }
+        });
+
+        ((Button) findViewById(R.id.btnVibePattern)).setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.O)
+            @Override
+            public void onClick(View view) {
+                long[] vibePattern = new long[]{0, 400, 200, 400};
+                Vibrate(vibePattern);
             }
         });
     }
 
     //Vibrate phone for X milliseconds
-    private void Vibrate(int milliseconds) {
+    private void Vibrate(VibrationEffect effect) {
         if (Build.VERSION.SDK_INT >= 26) {
-            ((Vibrator) getSystemService(VIBRATOR_SERVICE)).vibrate(VibrationEffect.createOneShot(milliseconds, VibrationEffect.DEFAULT_AMPLITUDE));
+            ((Vibrator) getSystemService(VIBRATOR_SERVICE)).vibrate(effect);
         } else {
-            ((Vibrator) getSystemService(VIBRATOR_SERVICE)).vibrate(milliseconds);
+            ((Vibrator) getSystemService(VIBRATOR_SERVICE)).vibrate(300);
+            Toast.makeText(this,"This Demo Is Not Fully Featured With APIS < 26", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    //Vibrate Phone With Effect
+    private void Vibrate(long[] effect) {
+        if (Build.VERSION.SDK_INT >= 26) {
+            ((Vibrator) getSystemService(VIBRATOR_SERVICE)).vibrate(effect, -1);
+        } else {
+            ((Vibrator) getSystemService(VIBRATOR_SERVICE)).vibrate(300);
+            Toast.makeText(this,"This Demo Is Not Fully Featured With APIS < 26", Toast.LENGTH_LONG).show();
         }
     }
 }
